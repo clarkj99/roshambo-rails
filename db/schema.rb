e8e19f8a7d83e367ccbd9a998b4d4806a4baaa73
clarkj99 <clark@stontech.net>
2019-11-15 12:17:57 -0500

    Redis Gem

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2019_11_15_053947) do
   create_table "players", force: :cascade do |t|
     t.string "username"
     t.string "display_name"
-    t.integer "current_level"
+    t.integer "current_level", default: 0
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
